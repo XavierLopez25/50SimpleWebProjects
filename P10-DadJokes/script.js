ffb24d112271aa7aa62c485b64a3f9ac2f984c5c
XavierLopez25 <lop22716@uvg.edu.gt>
2024-11-28 17:03:25 -0600

    feat: done P10-DadJokes

---
+++ P10-DadJokes/script.js
@@ -0,0 +1,17 @@
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+generateJoke();
+
+async function generateJoke() {
+  const res = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
+}
+
+jokeBtn.addEventListener("click", generateJoke);
