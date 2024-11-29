8cb8e6bc8623b8bbc5bf0fddb9a820e69f14936b
XavierLopez25 <lop22716@uvg.edu.gt>
2024-11-28 18:11:49 -0600

    feat: done P12-FAQCollapse

---
+++ P12-FAQCollapse/script.js
@@ -0,0 +1,8 @@
+const toggleButtons = document.querySelectorAll(".faq-toggle");
+console.log(toggleButtons);
+
+toggleButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.parentNode.classList.toggle("active");
+  });
+});
